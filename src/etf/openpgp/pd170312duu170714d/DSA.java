69c08f89cf7b9f14a4cac8ad23c26fa40cbb5e67
ugrinicu <62910441+ugrinicu@users.noreply.github.com>
2021-06-13 02:24:03 +0200

    prilagodjavanje

--- src/etf/openpgp/pd170312duu170714d/DSA.java
+++ src/etf/openpgp/pd170312duu170714d/DSA.java
@@ -20,106 +20,86 @@ import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPKeyPair;
+import org.bouncycastle.openpgp.PGPPublicKey;
+import org.bouncycastle.openpgp.operator.jcajce.JcaPGPKeyPair;
 
 public class DSA {
-	private KeyPair my_key_pair;
-	
-	public static byte[] generateSignature(PrivateKey ecPrivate_, byte[] input_) throws GeneralSecurityException
-	{
-		Signature signature = Signature.getInstance("SHA1withDSA", "BC");
-		signature.initSign(ecPrivate_);
-		signature.update(input_);
-		return signature.sign();
-	}
-	
-	public static boolean verifySignature(PublicKey ecPublic_, byte[] input_, byte[] encSignature_) throws GeneralSecurityException
-	{
-		Signature signature = Signature.getInstance("SHA1withDSA", "BC");
-		signature.initVerify(ecPublic_);
-		signature.update(input_);
-		return signature.verify(encSignature_);
-	}
-	
-	public void generate_keypair(int key_size_) {
-        try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DSA");
-            keyPairGenerator.initialize(key_size_);
-            this.my_key_pair = keyPairGenerator.generateKeyPair();
-        } catch (NoSuchAlgorithmException e) {
+    private KeyPair my_keypair;
+    protected PGPKeyPair my_pgp_keypair;
+    
+    public void setMy_pgp_keypair(PGPKeyPair my_pgp_keypair) {
+        this.my_pgp_keypair = my_pgp_keypair;
+    }
+
+    public void setMy_keypair(KeyPair my_keypair) {
+        this.my_keypair = my_keypair;
+    }
+
+    public PGPKeyPair getMy_pgp_keypair() {
+        return my_pgp_keypair;
+    }
+
+    public KeyPair getMy_keypair() {
+        return my_keypair;
+    }
+    
+    public void generate_keypair(int key_size_) {
+    try {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DSA");
+        keyPairGenerator.initialize(key_size_);
+        this.my_keypair = keyPairGenerator.generateKeyPair();
+        this.my_pgp_keypair = new JcaPGPKeyPair( PGPPublicKey.DSA, my_keypair, new Date() );
+    } catch (NoSuchAlgorithmException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }   catch (PGPException ex) {
+            Logger.getLogger(DSA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+}
+
+    public void export_keypair() {
+        try (FileOutputStream stream = new FileOutputStream("../dsa_keys.asc")) {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(stream));
+            bw.write(Base64.getEncoder().encodeToString(my_keypair.getPrivate().getEncoded()));
+            bw.newLine();
+            bw.write(Base64.getEncoder().encodeToString(my_keypair.getPublic().getEncoded()));
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
-	
-	public void export_keypair() {
-		  System.out.println("exported byte array: priv: " + KeyTools.bytesToHex(my_key_pair.getPrivate().getEncoded()));
-		  System.out.println("pub: " + KeyTools.bytesToHex(my_key_pair.getPublic().getEncoded()));
-		  try (FileOutputStream stream = new FileOutputStream("../dsa_keys.asc")) {
-			  	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(stream));
-			  	bw.write(KeyTools.bytesToHex(my_key_pair.getPrivate().getEncoded()));
-			  	bw.newLine();
-			  	bw.write(KeyTools.bytesToHex(my_key_pair.getPublic().getEncoded()));
-			  	bw.close();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	}
-	
-	public KeyPair import_keypair(String path) {
-		KeyPair imported = null;
-		if(path == null)path = "../dsa_keys.asc";
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(path));
-			String privateKeyString = reader.readLine();
-			String publicKeyString = reader.readLine();
-			byte[] privateKeyBytes = KeyTools.hexToBytes(privateKeyString);
-			byte[] publicKeyBytes = KeyTools.hexToBytes(publicKeyString);
-			System.out.println("imported byte array: priv :" + privateKeyString);
-			System.out.println("pub: " + publicKeyString);
-        	KeyFactory kf = KeyFactory.getInstance("DSA"); // or "EC" or whatever
-        	PKCS8EncodedKeySpec encodedKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
-			PrivateKey privateKey = kf.generatePrivate(encodedKeySpec);
-			EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
-			PublicKey publicKey = kf.generatePublic(publicKeySpec);
-			imported = new KeyPair(publicKey, privateKey);
-		} catch (IOException | InvalidKeySpecException | NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return imported;
-	}
-	
-	public static void main( String args[]) throws GeneralSecurityException {
-		Security.addProvider(new BouncyCastleProvider());
 
-		String msg = "We ready we ready we ready! FOR Y'ALL!";
-		DSA ldp = new DSA();
-		ldp.generate_keypair(1024);
-		System.out.println("Public key:" + ldp.my_key_pair.getPublic().getEncoded());
-		System.out.println("Private key:" + ldp.my_key_pair.getPrivate().getEncoded());
-		System.out.println("Potpisacemo poruku '" + msg + "'");
-		ldp.export_keypair();
-		KeyPair imported_kp = ldp.import_keypair(null);
-		if(ldp.my_key_pair.getPrivate().equals(imported_kp.getPrivate()) && ldp.my_key_pair.getPublic().equals(imported_kp.getPublic()))System.out.println("IMPORT YAY");
-		else System.out.println("IMPORT NAY");
-		byte[] signature = generateSignature(ldp.my_key_pair.getPrivate(), msg.getBytes());
-		
-		System.out.println("Potpis: " + signature);
-		
-		signature = generateSignature(ldp.my_key_pair.getPrivate(), msg.getBytes());
-		
-		System.out.println("Potpis: " + signature);
-		
-		boolean worked = verifySignature(ldp.my_key_pair.getPublic(), msg.getBytes(), signature);
-		
-		if (worked) {
-			System.out.println("Jupi!");
-		} else System.out.println("Bu hu!");
-	}
+    public void import_keypair(String path) {
+        if(path == null)path = "../dsa_keys.asc";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String privateKeyString = reader.readLine();
+            String publicKeyString = reader.readLine();
+            byte[] privateKeyBytes = Base64.getDecoder().decode(privateKeyString);
+            byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyString);
+            KeyFactory kf = KeyFactory.getInstance("DSA"); // or "EC" or whatever
+            PKCS8EncodedKeySpec encodedKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+            PrivateKey privateKey = kf.generatePrivate(encodedKeySpec);
+            EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+            PublicKey publicKey = kf.generatePublic(publicKeySpec);
+            my_keypair = new KeyPair(publicKey, privateKey);
+            this.my_pgp_keypair = new JcaPGPKeyPair( PGPPublicKey.DSA, my_keypair, new Date() );
+        } catch (IOException | InvalidKeySpecException | NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (PGPException ex) {
+            Logger.getLogger(DSA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- src/etf/openpgp/pd170312duu170714d/ElGamal.java
+++ src/etf/openpgp/pd170312duu170714d/ElGamal.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package etf.openpgp.pd170312duu170714d;
 
 import java.io.BufferedReader;
@@ -7,10 +12,8 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
 import java.security.AlgorithmParameterGenerator;
 import java.security.AlgorithmParameters;
-import java.security.GeneralSecurityException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyFactory;
 import java.security.KeyPair;
@@ -20,126 +23,101 @@ import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
-import java.security.Security;
 import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.InvalidParameterSpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-
-import javax.crypto.Cipher;
+import java.util.Base64;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.crypto.spec.DHParameterSpec;
+import org.bouncycastle.openpgp.*;
+import org.bouncycastle.openpgp.operator.jcajce.JcaPGPKeyPair;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
+/**
+ *
+ * @author Uros
+ */
 public class ElGamal {
-	private KeyPair my_key_pair;
-		
-	public static byte[] encrypt(PublicKey ecPublic_, byte[] input_) throws GeneralSecurityException
-	{
-		SecureRandom rand = new SecureRandom();
-		 
-		Cipher cipher = Cipher.getInstance("ElGamal", "BC");
-		cipher.init(Cipher.ENCRYPT_MODE, ecPublic_, rand);
-		return cipher.doFinal(input_);
-	}
-	
-	public static byte[] decrypt(PrivateKey ecPrivate_, byte[] encryptedData_) throws GeneralSecurityException
-	{
-		Cipher cipher = Cipher.getInstance("ElGamal", "BC");
-		cipher.init(Cipher.DECRYPT_MODE, ecPrivate_);
-		return cipher.doFinal(encryptedData_);
-	}
-	
-	public void generate_keypair(int size_) {
+
+    protected PGPKeyPair my_pgp_keypair;
+    private KeyPair my_keypair;
+    
+    
+    public void setMy_pgp_keypair(PGPKeyPair my_pgp_keypair) {
+        this.my_pgp_keypair = my_pgp_keypair;
+    }
+
+    public void setMy_keypair(KeyPair my_keypair) {
+        this.my_keypair = my_keypair;
+    }
+
+    public PGPKeyPair getMy_pgp_keypair() {
+        return my_pgp_keypair;
+    }
+
+    public KeyPair getMy_keypair() {
+        return my_keypair;
+    }
+
+    public void generate_keypair(int size_) {
         try {
-        	AlgorithmParameterGenerator a = AlgorithmParameterGenerator.getInstance("ElGamal", "BC");
+            AlgorithmParameterGenerator a = AlgorithmParameterGenerator.getInstance("ElGamal", "BC");
             a.init(size_, new SecureRandom());
             AlgorithmParameters params = a.generateParameters();
             DHParameterSpec elP = (DHParameterSpec)params.getParameterSpec(DHParameterSpec.class);
 
-        	KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ElGamal", "BC");
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ElGamal", "BC");
             keyPairGenerator.initialize(elP);
             KeyPair keyPair = keyPairGenerator.generateKeyPair();
-            this.my_key_pair = keyPair;
-        } catch (NoSuchAlgorithmException | NoSuchProviderException | InvalidAlgorithmParameterException e) {
+            this.my_keypair = keyPair;
+            this.my_pgp_keypair = new JcaPGPKeyPair( PGPPublicKey.ELGAMAL_ENCRYPT, my_keypair, new Date() );
+        } catch (NoSuchAlgorithmException | NoSuchProviderException | 
+                InvalidAlgorithmParameterException | InvalidParameterSpecException e) 
+        {} catch (PGPException ex) {
+            Logger.getLogger(ElGamal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void export_keypair() {
+        try (FileOutputStream stream = new FileOutputStream("../eg_keys.asc"); 
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(stream))) {
+            bw.write(Base64.getEncoder().encodeToString(my_keypair.getPrivate().getEncoded()));
+            bw.newLine();
+            bw.write(Base64.getEncoder().encodeToString(my_keypair.getPublic().getEncoded()));
+        } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (InvalidParameterSpecException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+
+        }
     }
 	
-	public void export_keypair() {
-		  try (FileOutputStream stream = new FileOutputStream("../eg_keys.asc")) {
-			  	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(stream));
-			  	bw.write(KeyTools.bytesToHex(my_key_pair.getPrivate().getEncoded()));
-			  	bw.newLine();
-			  	bw.write(KeyTools.bytesToHex(my_key_pair.getPublic().getEncoded()));
-			  	bw.close();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	}
-	
-	public KeyPair import_keypair(String path) {
-		KeyPair imported = null;
-		if(path == null)path = "../eg_keys.asc";
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(path));
-			String privateKeyString = reader.readLine();
-			String publicKeyString = reader.readLine();
-			byte[] privateKeyBytes = KeyTools.hexToBytes(privateKeyString);
-			byte[] publicKeyBytes = KeyTools.hexToBytes(publicKeyString);
-			System.out.println("imported byte array: priv :" + privateKeyString);
-			System.out.println("pub: " + publicKeyString);
-        	KeyFactory kf = KeyFactory.getInstance("ElGamal"); // or "EC" or whatever
-        	PKCS8EncodedKeySpec encodedKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
-			PrivateKey privateKey = kf.generatePrivate(encodedKeySpec);
-			EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
-			PublicKey publicKey = kf.generatePublic(publicKeySpec);
-			imported = new KeyPair(publicKey, privateKey);
-		} catch (IOException | InvalidKeySpecException | NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return imported;
-	}
-	
-
-	public static void main( String args[]) throws GeneralSecurityException {
-		Security.addProvider(new BouncyCastleProvider());
-
-		String msg = "We ready we ready we ready! FOR Y'ALL!";
-		ElGamal lg = new ElGamal();
-		lg.generate_keypair(512);
-		System.out.println("Enkriptovacemo poruku '" + new String(msg.getBytes(), StandardCharsets.UTF_8) + "'");
-		lg.export_keypair();
-		KeyPair imported_kp = lg.import_keypair(null);
-		if(lg.my_key_pair.getPrivate().equals(imported_kp.getPrivate()) && lg.my_key_pair.getPublic().equals(imported_kp.getPublic()))System.out.println("IMPORT YAY");
-		else System.out.println("IMPORT NAY");
-
-		byte[] encryptedData = encrypt(lg.my_key_pair.getPublic(), msg.getBytes());
-		
-		System.out.println("Enkriptovani podaci: " + encryptedData);
-		
-		byte[] decryptedData = decrypt(lg.my_key_pair.getPrivate(), encryptedData);
-		
-		System.out.println("Dekriptovani podaci: " + new String(decryptedData, StandardCharsets.UTF_8));
-		
-		boolean worked = true;
-		
-		for(int i=0; i<msg.getBytes().length; i++) {
-			if (decryptedData[i] != msg.getBytes()[i]) worked = false;
-		}
-		
-		if (worked) {
-			System.out.println("Jupi!");
-		} else System.out.println("Bu hu!");
-	}
+    public void import_keypair(String path) {
+            if(path == null)path = "../eg_keys.asc";
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(path));
+                String privateKeyString = reader.readLine();
+                String publicKeyString = reader.readLine();
+                byte[] privateKeyBytes = Base64.getDecoder().decode(privateKeyString);
+                byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyString);
+                System.out.println("imported byte array: priv :" + privateKeyString);
+                System.out.println("pub: " + publicKeyString);
+                KeyFactory kf = KeyFactory.getInstance("ElGamal"); // or "EC" or whatever
+                PKCS8EncodedKeySpec encodedKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+                PrivateKey privateKey = kf.generatePrivate(encodedKeySpec);
+                EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+                PublicKey publicKey = kf.generatePublic(publicKeySpec);
+                this.my_keypair = new KeyPair(publicKey, privateKey);
+                this.my_pgp_keypair = new JcaPGPKeyPair( PGPPublicKey.ELGAMAL_ENCRYPT, my_keypair, new Date() );
+            } catch (IOException | InvalidKeySpecException | NoSuchAlgorithmException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (PGPException ex) {
+            Logger.getLogger(ElGamal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
